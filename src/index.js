48e902336283e0bc19eeb814aff28781029a4865
joshn28 <joshnobleza@bell.net>
2020-11-09 22:59:30 -0500

    Shortened code and added a warning if a piano key wasn't pressed

--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 document.addEventListener("keydown", function (event) {
     if (event.code == "KeyA") {
         console.log("The 'A' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyS") {
+    } else if (event.code == "KeyS") {
         console.log("The 'S' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyD") {
+    } else if (event.code == "KeyD") {
         console.log("The 'D' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyF") {
+    } else if (event.code == "KeyF") {
         console.log("The 'F' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyG") {
+    } else if (event.code == "KeyG") {
         console.log("The 'G' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyH") {
+    } else if (event.code == "KeyH") {
         console.log("The 'H' key is pressed.");
-    }
-})
-document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyJ") {
+    } else if (event.code == "KeyJ") {
         console.log("The 'J' key is pressed.");
+    } else {
+        console.log("Warning: do not press this key.");
     }
 })
