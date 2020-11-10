332bc2e8fdc6bb48fa4b4da29ef15ca6bfaca6ff
joshn28 <joshnobleza@bell.net>
2020-11-10 10:53:25 -0500

    Added audio to event handlers when piano key is pressed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 document.addEventListener("keydown", function (event) {
-    if (event.code == "KeyA") {
-        console.log("The 'A' key is pressed.");
-    } else if (event.code == "KeyS") {
-        console.log("The 'S' key is pressed.");
-    } else if (event.code == "KeyD") {
-        console.log("The 'D' key is pressed.");
-    } else if (event.code == "KeyF") {
-        console.log("The 'F' key is pressed.");
-    } else if (event.code == "KeyG") {
-        console.log("The 'G' key is pressed.");
-    } else if (event.code == "KeyH") {
-        console.log("The 'H' key is pressed.");
-    } else if (event.code == "KeyJ") {
-        console.log("The 'J' key is pressed.");
+    if (event.code === "KeyA") {
+        let audio = new Audio("./audio/A.mp3");
+        audio.play();
+    } else if (event.code === "KeyS") {
+        let audio = new Audio("./audio/S.mp3");
+        audio.play();
+    } else if (event.code === "KeyD") {
+        let audio = new Audio("./audio/D.mp3");
+        audio.play();
+    } else if (event.code === "KeyF") {
+        let audio = new Audio("./audio/F.mp3");
+        audio.play();
+    } else if (event.code === "KeyG") {
+        let audio = new Audio("./audio/G.mp3");
+        audio.play();
+    } else if (event.code === "KeyH") {
+        let audio = new Audio("./audio/H.mp3");
+        audio.play();
+    } else if (event.code === "KeyJ") {
+        let audio = new Audio("./audio/J.mp3");
+        audio.play();
     } else {
         console.log("Warning: do not press this key.");
     }
