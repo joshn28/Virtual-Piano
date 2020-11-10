08c1999516bc0da1d8a9b4fff0fb31f5df994e6b
joshn28 <joshnobleza@bell.net>
2020-11-09 22:54:45 -0500

    Create event handlers for piano key presses

---
+++ src/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyA") {
+        console.log("The 'A' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyS") {
+        console.log("The 'S' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyD") {
+        console.log("The 'D' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyF") {
+        console.log("The 'F' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyG") {
+        console.log("The 'G' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyH") {
+        console.log("The 'H' key is pressed.");
+    }
+})
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyJ") {
+        console.log("The 'J' key is pressed.");
+    }
+})
