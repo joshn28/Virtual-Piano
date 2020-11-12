53940cb250a34f0815cb2f83bc707c0d88aca6d7
joshn28 <joshnobleza@bell.net>
2020-11-11 22:08:25 -0500

    Added event handlers for black piano keys

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 document.addEventListener("keydown", function (event) {
+    let audio;
     if (event.code === "KeyA") {
-        let audio = new Audio("./audio/A.mp3");
+        audio = new Audio("./audio/A.mp3");
         audio.play();
     } else if (event.code === "KeyS") {
-        let audio = new Audio("./audio/S.mp3");
+        audio = new Audio("./audio/S.mp3");
         audio.play();
     } else if (event.code === "KeyD") {
-        let audio = new Audio("./audio/D.mp3");
+        audio = new Audio("./audio/D.mp3");
         audio.play();
     } else if (event.code === "KeyF") {
-        let audio = new Audio("./audio/F.mp3");
+        audio = new Audio("./audio/F.mp3");
         audio.play();
     } else if (event.code === "KeyG") {
-        let audio = new Audio("./audio/G.mp3");
+        audio = new Audio("./audio/G.mp3");
         audio.play();
     } else if (event.code === "KeyH") {
-        let audio = new Audio("./audio/H.mp3");
+        audio = new Audio("./audio/H.mp3");
         audio.play();
     } else if (event.code === "KeyJ") {
-        let audio = new Audio("./audio/J.mp3");
+        audio = new Audio("./audio/J.mp3");
+        audio.play();
+    } else if (event.code === "KeyW") {
+        audio = new Audio("./audio/W.mp3");
+        audio.play();
+    } else if (event.code === "KeyE") {
+        audio = new Audio("./audio/E.mp3");
+        audio.play();
+    } else if (event.code === "KeyT") {
+        audio = new Audio("./audio/T.mp3");
+        audio.play();
+    } else if (event.code === "KeyY") {
+        audio = new Audio("./audio/Y.mp3");
+        audio.play();
+    } else if (event.code === "KeyU") {
+        audio = new Audio("./audio/U.mp3");
         audio.play();
     } else {
         console.log("Warning: do not press this key.");
